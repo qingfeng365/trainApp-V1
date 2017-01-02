08ded49a5e9d167d9655da755dc429cd1601a7f7
qingfengwuyou <h3658686@126.com>
2017-01-02 12:09:13 +0800

    no message

---
+++ www/js/app.router.js
@@ -0,0 +1,21 @@
+(function(){
+	'use strict';
+
+	var currApp = angular.module('main');
+
+	/**
+	 * 定义顶级缺省路由
+	 */
+	currApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider.state('intro',{
+			url:'/'
+		});
+
+	}]);
+
+
+})();
